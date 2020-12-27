a467c616dad0663e3f183b912e0f2c680e22dd68
ngahn <ngahn@vnpt.vn>
2020-12-27 13:54:49 +0700

    jja

---
+++ GIT_TEST/src/test/java/Hello.java
@@ -0,0 +1,12 @@
+/**
+ * @Author NgaHN
+ * @create 12/27/2020 1:34 PM
+ */
+public class Hello {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 5; i++){
+            System.out.println(i);
+        }
+    }
+}
